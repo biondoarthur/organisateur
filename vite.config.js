0f145c2379f1653221376c655bb68e627d59e6b7
Arthur Biondo <arthurbiondo@MacBook-Air-de-Arthur.local>
2026-08-14 13:23:50 +0200

    Serve installation page on Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'desktop' ? './' : '/',
+  build: {
+    rollupOptions: {
+      input: mode === 'desktop' ? 'index.html' : 'download.html',
+    },
+  },
 }))
